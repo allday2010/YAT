fb77f0c511b93af444b8901f19fe91cb6ab82ee3
yuan <allday2011@163.com>
2018-08-07 11:16:45 +0800

    YAT

---
+++ YAT/YAT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YAT
+//
+//  Created by Simon Blue on 2018/8/7.
+//  Copyright © 2018年 allday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YAT/YAT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YAT
+//
+//  Created by Simon Blue on 2018/8/7.
+//  Copyright © 2018年 allday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
